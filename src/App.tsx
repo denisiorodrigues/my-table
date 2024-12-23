9ef46d1f6f18450e3b10accc2b3f9eba337f1371
Denisio Rodrigues <denisio@ymail.com>
2024-12-23 17:02:35 -0300

    dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./components/ui/button"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, DialogClose } from "./components/ui/dialog"
 import { Input } from "./components/ui/input"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
       <h1 className="text-3xl font-bold">Produtos</h1>
-      
+
       <div className="flex items-center justify-between">
         <form className="flex items-center gap-2">
           <Input name="id" placeholder="ID do pedido" />
@@ -18,10 +19,39 @@ function App() {
             Filtrar resultados
           </Button>
         </form>
-        <Button>
-          <PlusCircle className="w-4 h-4 mr-2" />
-          Novo Produto
-        </Button>
+
+        <Dialog>
+          <DialogTrigger>
+            <Button>
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Novo Produto
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Novo Produto</DialogTitle>
+              <DialogDescription>Criar um novo produto no sistema</DialogDescription>
+
+              <form className="space-y-6">
+                <div className="grid grid-cols-6 items-center text-right gap-3">
+                  <label htmlFor="name">Produto</label>
+                  <Input id="name" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-6 items-center text-right gap-3">
+                  <label htmlFor="price">Proço</label>
+                  <Input id="price" className="col-span-3" />
+                </div>
+
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button" variant="outline">Cancelar</Button>
+                  </DialogClose>
+                  <Button type="submit">Salvar</Button>
+                </DialogFooter>
+              </form>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
       
       <div className="border rounded-lg p-2">  
